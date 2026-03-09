03f92020412a23ce7b3099d12b1e202102986312
Tran Thi Kim Ngan <trananhthy615@gmail.com>
2026-03-09 15:50:33 +0700

    level 2 - validation filter search pagination

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-# lưu todo trong RAM
+# lưu todos trong RAM
 todos = []
 
-# model dữ liệu
+# Model dữ liệu
 class Todo(BaseModel):
     id: int
-    title: str
+    title: str = Field(min_length=3, max_length=100)
     is_done: bool = False
 
 
@@ -23,20 +23,46 @@ def root():
     return {"message": "Hello ToDo API"}
 
 
-# CREATE
+# CREATE TODO
 @app.post("/todos")
 def create_todo(todo: Todo):
     todos.append(todo)
     return todo
 
 
-# READ ALL
+# GET TODOS (filter + search + pagination)
 @app.get("/todos")
-def get_todos():
-    return todos
+def get_todos(
+    q: str = None,
+    is_done: bool = None,
+    limit: int = 10,
+    offset: int = 0
+):
+
+    result = todos
+
+    # search title
+    if q:
+        result = [t for t in result if q.lower() in t.title.lower()]
+
+    # filter is_done
+    if is_done is not None:
+        result = [t for t in result if t.is_done == is_done]
+
+    total = len(result)
+
+    # pagination
+    result = result[offset: offset + limit]
+
+    return {
+        "items": result,
+        "total": total,
+        "limit": limit,
+        "offset": offset
+    }
 
 
-# READ ONE
+# GET ONE TODO
 @app.get("/todos/{todo_id}")
 def get_todo(todo_id: int):
     for t in todos:
@@ -45,7 +71,7 @@ def get_todo(todo_id: int):
     raise HTTPException(status_code=404, detail="Todo not found")
 
 
-# UPDATE
+# UPDATE TODO
 @app.put("/todos/{todo_id}")
 def update_todo(todo_id: int, todo: Todo):
     for i, t in enumerate(todos):
@@ -55,7 +81,7 @@ def update_todo(todo_id: int, todo: Todo):
     raise HTTPException(status_code=404, detail="Todo not found")
 
 
-# DELETE
+# DELETE TODO
 @app.delete("/todos/{todo_id}")
 def delete_todo(todo_id: int):
     for i, t in enumerate(todos):
